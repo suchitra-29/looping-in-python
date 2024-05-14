316f7142c9f18120ef12b3a225af4858224ed96e
suchitra-29 <153913890+suchitra-29@users.noreply.github.com>
2024-05-14 21:01:57 +0530

    Add files via upload

---
+++ another reverse star.py
@@ -0,0 +1,9 @@
+n=int(input("enter number of rows"))
+for i in range(1,n+1):
+    print("*"*((n+1)-i))
+
+
+n=int(input("enter number of rows"))
+for i in range(1,n+1):
+    print("*"*i)
+
---
+++ printing stars in different ways.py
@@ -0,0 +1,19 @@
+n=int(input("enter number of rows"))
+for i in range(1,n+1):
+    print("*"*i)
+print("this is right angle star ")
+
+
+n=int(input("enter number of rows"))
+for i in range(1,n+1):
+    print("*"*((n+1)-i))
+print("this is reverse right angle")
+
+
+
+n=int(input("enter number of rows"))
+for i in range(n):
+    print("*"*n)
+print("this is n*n format stars")
+    
+
